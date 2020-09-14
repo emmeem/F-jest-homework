8d07cdc56225c83d0d705f15069c70bcb485dd34
emmeem <1124782639@qq.com>
2020-09-14 22:59:47 +0800

    finish user test js

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,4 +1,5 @@
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
@@ -13,9 +14,14 @@ describe("register", () => {
     await expect(result).resolves.toEqual({})
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockReturnValue(false);
+    const username = "mocked username";
+    const password = "mock password";
 
-    
+    const result = register(username, password);
+
+    await expect(result).rejects.toThrowError("wrong username or password")
   });
 });
