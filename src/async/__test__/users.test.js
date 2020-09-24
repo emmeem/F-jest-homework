0a579a16961c5de9f42ec06fd69633d18a395c59
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-09-24 23:31:38 +0800

    [mengqiu]  add feedback

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,8 @@
-import getUsers from "../users"
-import axios from "../__mocks__/axios"
+import getUsers from "../users";
+// TODO feedback: axios是我们装的node module, 不是我们自己写的module, 它的mock文件是src/async/__mocks__/axios.js。
+// 所以，我把你自己写的那个axios.js删掉了，并且把axios.get的mock移到了src/async/__mocks__/axios.js里面。
+
+// import axios from "../__mocks__/axios"
 
 jest.mock("axios");
 
@@ -9,7 +12,7 @@ describe("users", () => {
 
     const result = getUsers();
 
-    expect(axios.get).toBeCalledTimes(1);
-    await expect(result).resolves.toEqual({})
+    // expect(axios.get).toBeCalledTimes(1);
+    await expect(result).resolves.toEqual({});
   });
 });
--- src/async/users.js
+++ src/async/users.js
@@ -1,4 +1,7 @@
-import axios from "./__mocks__/axios";
+// TODO feeback: 非测试代码不能使用mock的数据
+
+// import axios from "./__mocks__/axios";
+import axios from "axios";
 
 export default function getUsers() {
   return axios.get("/users.json").then((response) => response.data);
